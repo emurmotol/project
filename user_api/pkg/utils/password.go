0a89f4d9c562c60a3b6dd58215be2a4c40b1c9fe
emur <emar@hybridblock.io>
2019-03-08 01:19:38 +0800

    remove unused

--- user_api/pkg/utils/password.go
+++ user_api/pkg/utils/password.go
@@ -8,8 +8,3 @@ func HashPassword(password string) (string, error) {
 	bytes, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
 	return string(bytes), err
 }
-
-func CheckPasswordHash(password, hash string) bool {
-	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
-	return err == nil
-}
