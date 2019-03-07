87308564e6e7e577d6ecfcc6c028002f7193a55f
emur <emar@hybridblock.io>
2019-03-08 01:05:20 +0800

    rm not needed

--- user_api/pkg/utils/jwt.go
+++ user_api/pkg/utils/jwt.go
@@ -1,26 +1,8 @@
 package utils
 
 import (
-	"io/ioutil"
-
 	stdjwt "github.com/dgrijalva/jwt-go"
 )
-
-func GenerateJWTToken(claims stdjwt.Claims, privateKeyPath string) (string, error) {
-	key, err := ioutil.ReadFile(privateKeyPath)
-	if err != nil {
-		return "", err
-	}
-	parsedKey, err := stdjwt.ParseRSAPrivateKeyFromPEM(key)
-	if err != nil {
-		return "", err
-	}
-	token, err := stdjwt.NewWithClaims(stdjwt.SigningMethodRS256, claims).SignedString(parsedKey)
-	if err != nil {
-		return "", err
-	}
-	return token, nil
-}
 
 type JWTClaims struct {
 	stdjwt.StandardClaims
